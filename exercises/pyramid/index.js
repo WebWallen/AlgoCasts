4646d368b842cf7aaadd0c7fdc913b680e75aac8
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-02-19 20:20:18 -0500

    Hope I never encounter these two in the wild. Rough to say the least.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,45 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    // Calculate the midpoint and assign to a variable
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    // For each row with a smaller value than n...
+    for (let row = 0; row < n; row++) {
+        // Assign an empty string to level
+        let level = '';
+        // For each column with a smaller value than the midpoint...
+        for (let column = 0; column < (2 * n - 1); column++) {
+            // If the column value is greater than or equal to (midpoint - row) and less than or equal to the row value...
+            if (midpoint - row <= column && midpoint + row >= column) {
+                // Add a block (#) to the pyramid level
+                level += '#';
+            // Otherwise...
+            } else {
+                // Add an space
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+} 
+pyramid(3);
+
+
+function pyramidRecursive(n, row = 0, level ='') {
+    if (row === n) return
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    // For each row that's less than n...
+    for (let row = 0; row < n; row++) {
+        // Assign an empty string to stair
+        stair = '';
+        // For each column of that's less than n...
+        for (let column = 0; column < n; column++) {
+            // If the column is equal to or less than the row...
+            if (column <= row) stair += '#'; // ...add a step (#) to the staircase
+            else stair += ' '; // If the column is greater than row, add a space to the staircase
+        }
+        // After the inner loop is complete, print each stair (inside outer loop so we can four print statements)
+        console.log(stair);
+    }
+}
+
+// Good practice to use default values for arguments on recursive functions (lowers risk of infinite loops)
+function steps_recursive(n, row = 0, stairs = '') {
+    // Base case: if n equals the row value, return
+    if (n === row) return;
+    // If n is equal to the amount of stairs...
+    if (n === stairs.length) {
+        // Print the stairs
+        console.log(stairs);
+        // Return a recursive call and increment row
+        return steps_recursive(n, row + 1);
+    }
+    // If we haven't reached the row value, add steps (#) until we have
+    if (stairs.length <= row) stairs += '#';
+    // Otherwise, add an empty strength
+    else stairs += ' ';
+    // Recursively call the function until n equals row
+    steps_recursive(n, row, stairs);
+}
+
+steps_recursive(5);
 
 module.exports = steps;
