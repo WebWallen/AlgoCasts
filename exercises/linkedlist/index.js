a519dd0321fb61b3c570bebab7413692119e08ab
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-03-26 12:13:48 -0400

    Repeated all the linked list problems for HR interview practice.

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -4,199 +4,198 @@
 
 class Node {
     constructor(data, next = null) {
-        // Default data storage property for attached value
         this.data = data;
-        // Points to next node on LL (unless tail, then null)
         this.next = next;
     }
 }
 
+// All this needs to know is head, contrains next reference to rest
 class LinkedList {
-    // All this knows by default (without method) is the head
     constructor() {
-        // Head is stored with list.head = Node(1)
         this.head = null;
     }
-    // insertFirst adds a node to front of LL
+
+    // Create brand new node with data, insert to head
+    // An INSERT method, not an OVERWRITE method
+    // Insert new node and repair head reference
     insertFirst(data) {
-        // Create new node, assign to head, pass in previous head as next
         this.head = new Node(data, this.head);
     }
-    // Find the size of LL
+
+    // Iterate through the LL and increment counter variable
     size() {
-        // Initialize empty count variable
         let count = 0;
-        // Assign current head to node
-        let node = this.head;
-        // While there is a node...
-        while (node) {
-            // Increment count +1
+        let current = this.head;
+        while (current) {
             count++;
-            // Reassign node to next node
-            node = node.next;
+            current = current.next;
         }
-        // Once tail is reached, return count
         return count;
     }
-    // Get first node in LL
+
+    // First node is the head, simply return it
     getFirst() {
-        // Return the head
         return this.head;
     }
-    // Get last node in LL
+
     getLast() {
-        // If there's no head (or any nodes), return null
+        // No head, no last, return null
         if (!this.head) return null;
-        // Assign current node to head
-        let node = this.head;
-        // While there is a node...
-        while (node) {
-            // If there isn't a next node, return node (tail)
-            if (!node.next) return node;
-            // Increment and reassign node until we reach end
-            node = node.next;
+
+        let current = this.head;
+
+        while (current) {
+            // Conditional must come before loop!
+            // No next ref means it is last node
+            if (!current.next) return current;
+            current = current.next;
         }
     }
-    // Clear the linked list of all nodes
+
+    // LL only knows about head, make it null and rest vanish
     clear() {
-        // Head is only way it's aware of other nodes, so make it null
         this.head = null;
     }
-    // Remove the first node (head) from our LL
+
+    // Reassign head property to next so it points at 2nd node
     removeFirst() {
-        // If there's not head, just return (nothing to remove)
         if (!this.head) return;
-        // Assign current head to the next node to turn into new head
         this.head = this.head.next;
     }
-    // Remove the last node (tail) from our LL
+
+    // Use previous/current pointers to remove last reference
     removeLast() {
-        // If the list is completely empty, exit (return)
         if (!this.head) return;
-        // If the list only has one node...
+
         if (!this.head.next) {
-            // Reassign the head as null
             this.head = null;
-            // Return (exit) the function
             return;
         }
-        // Otherwise, assign some placeholder variables to iterate with
+
         let previous = this.head;
-        let node = this.head.next;
-        // While there is a next node...
-        while (node.next) {
-            // Increment each variable (moves each to the right one space)
-            previous = node;
-            node = node.next;
+        let current = this.head.next;
+
+        while (current.next) {
+            previous = current;
+            current = current.next;
         }
-        // When the node reaches the end of the line, set previous.next to null
+
         previous.next = null;
-        // Above effectively deletes the last node by removing the pointer to it
     }
-    // Insert a new tail to the end of our LL
+
+    // Find current last and add new node as current's next
     insertLast(data) {
-        // Get the last node and assign to last
-        const last = this.getLast();
-        // If there are nodes to check...
-        if (last) {
-            // Assign the new node to previous tail's next
-            last.next = new Node(data);
-        }
-        else {
-        // Otherwise, assign the new node to head (LL is empty)
-            this.head = new Node(data);
-        }
+        let last = this.getLast();
+        if (last) last.next = new Node(data);
+        else this.head = new Node(data);
     }
-    // Get the node at a given index (starting at 0)
+
+    // Return entire node associated with a given index
     getAt(index) {
-        // Assign empty counter variable
-        let counter = 0;
-        // Assign head to current node
-        let node = this.head;
-        // While there are node...
-        while (node) {
-            // If we're reached the index
-            if (counter === index) {
-                // Return the node
-                return node;
-            }
-            // Otherwise, increment counter
-            counter++;
-            // Add next to node and loop
-            node = node.next;
+        let count = 0;
+        let current = this.head;
+
+        while (current) {
+            if (count === index) return current;
+            count++;
+            current = current.next;
         }
-        // Didn't find the index, so return null
+
         return null;
     }
-    // Remove the node at a given index
+
+    // Requires many more edge cases in case of errors
     removeAt(index) {
-        // Empty list = return
+        // No nodes
         if (!this.head) return;
-        // If index is the head...
+        // Removing index 0 (doesn't mean no elements, removing first)
         if (index === 0) {
-            // Remove by assigning head to next
-            this.head = this.head.next 
-            // Exit function
+            this.head = this.head.next;
             return;
         }
-        // Get the previous node (index - 1)
-        const previous = this.getAt(index - 1);
-        // If there's not a previous node or one at index position, return (nothing to remove)
+        // Selecting previous node
+        let previous = this.getAt(index - 1);
+        // Error handling for index outside of LL range
         if (!previous || !previous.next) return;
-        // Otherwise, update the previous' pointer so it skips removed index (add an extra next)
+        // Passed all the error handling nexts, so increment
         previous.next = previous.next.next;
     }
-    // Insert the node at a given index
+
+    // Not overwriting any data, merely repairing connections
     insertAt(data, index) {
+        // No nodes
         if (!this.head) {
             this.head = new Node(data);
             return;
         }
+
+        // Zero index
         if (index === 0) {
-            // Node class has next node prop = use to scoot previous head
             this.head = new Node(data, this.head);
-            return;
         }
-        // Get the previous index; if no previous, get last node
-        const previous = this.getAt(index - 1) || this.getLast();
-        // Assign the new node and point to the previous' next
-        const node = new Node(data, previous.next);
-        // Modify the previous' next so it points to new node
-        previous.next = node;
-    }
 
-    // Interview Pro-Tip: ask if it's okay to condense methods by using them in a generic, multi-purpose way
-    intertFirstB(data) {
-        this.insertAt(data, 0);
-    }
+        // Insert anywhere in middle
+        // Second part is for handling insertion at the end
+        let previous = this.getAt(index - 1) || this.getLast();
+        let current = new Node(data, previous.next);
+        previous.next = current;
 
-    getFirstB() {
-        return this.getAt(0);
-    }
-
-    getLastB() {
-        return this.getAt(this.size() - 1);
-    }
-
-    // Generator Content Below = bonus/optional, would need further research
-
-    forEach(fn) {
-        let node = this.head;
-        let counter = 0;
-        while (node) {
-            fn(node, counter);
-            node = node.next;
-            counter++;
-        }
-    }
-
-    *[Symbol.iterator]() {
-        let node = this.head;
-        while (node) {
-            yield node;
-            node = node.next;
-        }
+        // Out of bounds
+        if (!previous || previous.next); 
     }
 }
+
+//     // Insert the node at a given index
+//     insertAt(data, index) {
+//         if (!this.head) {
+//             this.head = new Node(data);
+//             return;
+//         }
+//         if (index === 0) {
+//             // Node class has next node prop = use to scoot previous head
+//             this.head = new Node(data, this.head);
+//             return;
+//         }
+//         // Get the previous index; if no previous, get last node
+//         const previous = this.getAt(index - 1) || this.getLast();
+//         // Assign the new node and point to the previous' next
+//         const node = new Node(data, previous.next);
+//         // Modify the previous' next so it points to new node
+//         previous.next = node;
+//     }
+
+//     // Interview Pro-Tip: ask if it's okay to condense methods by using them in a generic, multi-purpose way
+//     intertFirstB(data) {
+//         this.insertAt(data, 0);
+//     }
+
+//     getFirstB() {
+//         return this.getAt(0);
+//     }
+
+//     getLastB() {
+//         return this.getAt(this.size() - 1);
+//     }
+
+//     // Generator Content Below = bonus/optional, would need further research
+
+//     forEach(fn) {
+//         let node = this.head;
+//         let counter = 0;
+//         while (node) {
+//             fn(node, counter);
+//             node = node.next;
+//             counter++;
+//         }
+//     }
+
+//     *[Symbol.iterator]() {
+//         let node = this.head;
+//         while (node) {
+//             yield node;
+//             node = node.next;
+//         }
+//     }
+// }
 
 module.exports = { Node, LinkedList };
